e1e7948d82d890cf487c2f269717961eef7d0936
Noel Andia Flores <noel.andiaf@gmail.com>
2025-01-07 21:05:53 -0400

    Últimas modificaciones sobre envío de funciones a componentes.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,35 +1,25 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
-
-const Button = ({onClick, text}) => (
-  <button onClick={onClick}>
-    {text}
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
   </button>
 )
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [value, setValue] = useState(10)
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button
-        onClick={increaseByOne}
-        text = 'plus'
-      />
-      <Button 
-        onClick={setToZero}
-        text = 'zero'
-      />
-      <Button 
-        onClick={decreaseByOne}
-        text = 'minus'
-      />
+      {value}
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
